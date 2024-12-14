7c9ac0d6a3d6d28d5babbc06122329c2749e3bf1
JalYt-Mej-Kal <kuba.cholewa02@gmail.com>
2024-12-14 20:25:54 +0100

    added Order class

---
+++ Order.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_promise
+{
+    internal class Order
+    {
+        private Dictionary<string, OrderItem> _items = new Dictionary<string, OrderItem>();
+    
+        public void AddProduct(Product product, int quantity)
+        {
+            var existingItem = _items.TryAdd(product.Name, new OrderItem(product, quantity));
+            if (!existingItem)
+            {
+                if (_items.TryGetValue(product.Name, out var orderItem))
+                {
+                    orderItem.Quantity += quantity;
+                }
+            }
+        }
+
+        public void RemoveProduct(Product product, int quantity)
+        {
+            if (_items.ContainsKey(product.Name))
+            { 
+                _items.Remove(product.Name);
+            }
+            else
+            {
+                Console.WriteLine("Dany produkt nie znajduje się w zamówieniu");
+            }
+        }
+    }
+}
