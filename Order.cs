0ed520091e3d2dd2b8a18d3cbf412dcc280520e7
JalYt-Mej-Kal <kuba.cholewa02@gmail.com>
2024-12-15 21:13:28 +0100

    -changed discount logic -refactored all classes

--- Order.cs
+++ Order.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace project_promise
 {
     internal class Order
     {
-        public Dictionary<string, OrderItem> orderItems = new Dictionary<string, OrderItem>();
+        public Dictionary<string, OrderItem> orderItems = new Dictionary<string, OrderItem>(StringComparer.OrdinalIgnoreCase);
 
-        private Dictionary<string, Product> _products = new Dictionary<string, Product>
+        private Dictionary<string, Product> _products = new Dictionary<string, Product>(StringComparer.OrdinalIgnoreCase)
         {
             { "Laptop", new Product("Laptop", 2500m) },
             { "Klawiatura", new Product("Klawiatura", 120m) },
@@ -19,18 +15,17 @@ namespace project_promise
             { "Kaczka debuggująca", new Product("Kaczka debuggująca", 66m) }
         };
 
-
         public void AddProduct(string productName, int quantity)
         {
             if (orderItems.TryGetValue(productName, out var orderItem))
             {
                 orderItem.Quantity += quantity;
-                Console.WriteLine($"Zamówienie dla produktu \"{productName}\" zostało zwiększone o {quantity}. Pozostało {orderItem.Quantity}.");
+                Console.WriteLine($"Zamówienie dla produktu: {productName} zostało zwiększone o {quantity}. Pozostało {orderItem.Quantity}.");
             }
             else
             {
                 orderItems[productName] = new OrderItem(_products[productName], quantity);
-                Console.WriteLine($"Dodano produkt \"{productName}\" do zamówienia. Ilość: {quantity}.");
+                Console.WriteLine($"Dodano produkt: {productName} do zamówienia. Ilość: {quantity}.");
             }
         }
 
@@ -47,7 +42,7 @@ namespace project_promise
                 else
                 {
                     orderItem.Quantity -= quantity;
-                    Console.WriteLine($"Zamówienie dla produktu \"{productName}\" zostało zmniejszone o {quantity}. Pozostało {orderItem.Quantity}.");
+                    Console.WriteLine($"Zamówienie dla produktu \"{productName}\" zostało zmniejszone o {quantity}. Ilość: {orderItem.Quantity}.");
                 }
             }
             else
@@ -56,7 +51,44 @@ namespace project_promise
             }
         }
 
+        public (decimal totalPrice, decimal totalDiscount) CalculateTotalPrice()
+        {
+            decimal totalOrderDiscount = 0;
 
+            var sortedProducts = orderItems.Values
+                        .SelectMany(order => Enumerable.Repeat(order.Product, order.Quantity))
+                        .OrderBy(product => product.Price)
+                        .ToList();
+
+            decimal discount = 0;
+
+            if (sortedProducts.Count >= 2)
+            {
+                var secondCheapest = sortedProducts[0];
+                discount = secondCheapest.Price * 0.10m;
+            }
+
+            if (sortedProducts.Count >= 3)
+            {
+                var thirdCheapest = sortedProducts[0];
+                decimal alternativeDiscount = thirdCheapest.Price * 0.20m;
+                if (alternativeDiscount > discount)
+                {
+                    discount = alternativeDiscount;
+                }
+            }
+
+            decimal totalPrice = sortedProducts.Sum(product => product.Price);
+            totalPrice -= discount;
+
+            if (totalPrice > 5000)
+            {
+                totalOrderDiscount = 0.05m * totalPrice;
+                totalPrice = totalPrice - totalOrderDiscount;
+            }
+            
+            return (totalPrice, totalOrderDiscount + discount);
+        }
         public bool CheckIfProductExists(string productName)
         {
             if (_products.ContainsKey(productName))
--- Program.cs
+++ Program.cs
@@ -1,21 +1,80 @@
 ﻿using project_promise;
-using System;
-using System.ComponentModel.DataAnnotations;
-using System.Security.Cryptography.X509Certificates;
-using System.Xml.Serialization;
+
 
 class Program
 {
-    private static string ProductName { get; set; }
-    private static int ProductQty {  get; set; }
-	public static void ProductValidation(Order order)
+    private static string ProductName { get; set; } = string.Empty;
+    private static int ProductQty { get; set; }
+
+    public static void Main(string[] args)
+    {
+        bool exit = false;
+        var choice = string.Empty;
+        var order = new Order();
+
+        while (!exit)
+        {
+            Console.WriteLine("1. Dodaj produkt do zamówienia");
+            Console.WriteLine("2. Usuń produkt z zamówienia");
+            Console.WriteLine("3. Sprawdź wartość zamówienia");
+            Console.WriteLine("4. Wyjdź");
+
+            choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    {
+                        Console.Clear();
+                        ProductValidation(order);
+                        order.AddProduct(ProductName, ProductQty);
+                        
+                        break;
+                    }
+                case "2":
+                    {
+                        Console.Clear();
+                        ProductValidation(order);
+                        order.RemoveProduct(ProductName, ProductQty);
+
+                        break;
+                    }
+                case "3":
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Lista produktów: ");
+                        foreach (var product in order.orderItems)
+                        {
+                            Console.WriteLine($"Produkt: {product.Value.Product.Name}, ilość: {product.Value.Quantity}, cena: {product.Value.TotalPrice()}");
+                        }
+                        var (totalPrice, totalDiscount) = order.CalculateTotalPrice();
+                        Console.WriteLine($"Suma: {Math.Round(totalPrice, 2)}");
+                        Console.WriteLine($"Rabat wynosi: {Math.Round(totalDiscount, 2)}");
+
+                        break;
+                    }
+                case "4":
+                    {
+                        exit = true;
+
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Nieprawidłowy wybór. Spróbuj ponownie.");
+                        break;
+                    }
+            }
+        }
+    }
+
+    public static void ProductValidation(Order order)
 	{
         bool correctProduct = false;
         bool correctProductQty = false;
         do
         {
             Console.WriteLine("Podaj nazwę produktu: ");
-            ProductName = Console.ReadLine();
+            ProductName = Console.ReadLine() ?? string.Empty;
             correctProduct = order.CheckIfProductExists(ProductName);
             if (!correctProduct)
             {
@@ -40,93 +99,4 @@ class Program
         }
         while (!correctProductQty);
     }
-	public static void Main(string[] args)
-	{
-		bool exit = false;
-		var choice = string.Empty;
-        var order = new Order();
-
-        while (!exit)
-		{
-			Console.WriteLine("1. Dodaj produkt do zamówienia");
-			Console.WriteLine("2. Usuń produkt z zamówienia");
-			Console.WriteLine("3. Sprawdź wartość zamówienia");
-			Console.WriteLine("4. Wyjdź");
-
-			choice = Console.ReadLine();
-            switch (choice)
-			{
-				case "1":
-					{
-                        ProductValidation(order);
-						
-						order.AddProduct(ProductName, ProductQty);
-
-						break;
-					}
-				case "2":
-					{
-                        ProductValidation(order);
-
-                        order.RemoveProduct(ProductName, ProductQty);
-
-                        break;
-					}
-				case "3":
-					{
-						decimal totalOrderDiscount = 0;
-
-                        var sortedProducts = order.orderItems.Values
-                                    .SelectMany(order => Enumerable.Repeat(order.Product, order.Quantity))
-                                    .OrderBy(product => product.Price)
-                                    .ToList();
-
-                        decimal discount = 0;
-
-                        if (sortedProducts.Count >= 2)
-                        {
-                            var secondCheapest = sortedProducts[0];
-                            discount = secondCheapest.Price * 0.10m;
-                        }
-
-                        if (sortedProducts.Count >= 3)
-                        {
-                            var thirdCheapest = sortedProducts[0];
-                            decimal alternativeDiscount = thirdCheapest.Price * 0.20m;
-                            if (alternativeDiscount > discount)
-                            {
-                                discount = alternativeDiscount;
-                            }
-                        }
-
-                        decimal totalPrice = sortedProducts.Sum(product => product.Price);
-                        totalPrice -= discount;
-
-                        foreach (var product in order.orderItems)
-						{
-							Console.WriteLine($"Produkt: {product.Value.Product.Name}, ilość: {product.Value.Quantity}, cena: {product.Value.TotalPrice()}");
-						}
-
-						if (totalPrice > 5000)
-						{
-							totalOrderDiscount = 0.05m * totalPrice;
-                            totalPrice = totalPrice - totalOrderDiscount;
-						}
-						Console.WriteLine($"Suma: {Math.Round(totalPrice, 2)}");
-                        Console.WriteLine($"Rabat wynosi: {Math.Round(totalOrderDiscount + discount, 2)}");
-                        break;
-					}
-				case "4":
-					{
-						exit = true;
-						break;
-					}
-				default:
-					{
-						Console.WriteLine("Nieprawidłowy wybór. Spróbuj ponownie.");
-						break;
-					}
-			}
-		}
-	}
 }
